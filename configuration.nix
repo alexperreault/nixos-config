270b276d161fee9be7e7050d972c2e10945280f5
Alexandre Perreault <alexpqc@proton.me>
2026-07-03 20:31:40 -0400

    45

--- configuration.nix
+++ configuration.nix
@@ -143,6 +143,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    alsa-utils
     bibata-cursors
     clang
     discord
